b305b6b36a11a74f2a2b6f8ce5aaaafd9ccff110
drzippit <drzippit@doctorzipp.it>
2024-09-05 11:47:28 +0200

    init

---
+++ 2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "os"
+  "fmt"
+)
+
+func main() {
+  input := os.Args[1]
+
+  if input == "one" {
+    fmt.Println("1")
+  }
+  if input == "two" {
+    fmt.Println("2")
+  }
+  if input == "three" {
+    fmt.Println("3")
+  }
+  if input == "four" {
+    fmt.Println("4")
+  }
+  if input == "five" {
+    fmt.Println("5")
+  }
+  if input == "six" {
+    fmt.Println("6")
+  }
+  if input == "seven" {
+    fmt.Println("7")
+  }
+  if input == "eight" {
+    fmt.Println("8")
+  }
+  if input == "nine" {
+    fmt.Println("9")
+  }
+  if input == "ten" {
+    fmt.Println("10")
+  }
+}
